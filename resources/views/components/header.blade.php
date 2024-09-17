dde19a2064fba3afdbb8806730b827354e31ae0b
Jully05 <julyanafayumihidayat@gmail.com>
2024-09-17 08:52:34 +0700

    Initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,8 @@
+<header class="bg-blue-500 shadow min-h-full flex justify-center items-center">
+    <div
+        class=" flex flex-col justify-center items-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 min-h-[200px]">
+        <img class="w-[120px] h-[120px]" src="img/logo-bmti.png" alt="Logo">
+        <h1 class="text-5xl font-inter font-bold text-neutral-50 mt-[60px] tracking-wider">PNBP</h1>
+        <h2 class="text-xl font-inter text-neutral-50 tracking-wider">Penerimaan Negara Bukan Pajak</h2>
+    </div>
+</header>
